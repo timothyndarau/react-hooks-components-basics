42ece20d29a6744a477ad3c2b7ce34f5dfd1b097
timothy <timothy.ndarau@student.moringaschool.com>
2023-07-18 10:39:32 +0300

    lab components done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
-
+import Nav from "./nav";
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Nav />
     </div>
   );
 }
---
+++ src/nav.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Nav()  {
+    return(
+        <h1>this is my nav bar</h1>
+    )
+}
+ export default Nav 
